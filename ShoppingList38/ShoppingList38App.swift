18197d068090e8c766f2531ae7498684e829159f
Nikita Tsomuk <tsomuk@me.com>
2026-09-07 19:51:06 +0300

    Initial Commit

---
+++ ShoppingList38/ShoppingList38App.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingList38App.swift
+//  ShoppingList38
+//
+//  Created by Nikita Tsomuk on 07.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingList38App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
