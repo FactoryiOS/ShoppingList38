b3ebeadde3ef6124618adc6e3e18d8e5773cf2ca
elmkh1IL <el.mkh@inbox.ru>
2026-09-11 16:29:20 +0300

    [Feature] Add colors, images and typography

---
+++ ShoppingList38/Resources/AppTypography.swift
@@ -0,0 +1,41 @@
+//
+//  AppTypography.swift
+//  ShoppingList38
+//
+//  Created by el on 11.09.2026.
+//
+
+import SwiftUI
+
+enum AppTypography {
+    
+    static let headline = Font.system(size: 17, weight: .medium)
+    static let body = Font.system(size: 17, weight: .regular)
+    
+    static let largeTitleRegular = Font.system(size: 34, weight: .regular)
+    static let largeTitleSemibold = Font.system(size: 34, weight: .semibold)
+    
+    static let title1Regular = Font.system(size: 28, weight: .regular)
+    static let title1Semibold = Font.system(size: 28, weight: .semibold)
+    
+    static let title2Regular = Font.system(size: 22, weight: .regular)
+    static let title2semibold = Font.system(size: 22, weight: .semibold)
+    
+    static let title3Regular = Font.system(size: 20, weight: .regular)
+    static let title3Semibold = Font.system(size: 20, weight: .semibold)
+    
+    static let calloutRegular = Font.system(size: 16, weight: .regular)
+    static let calloutSemibold = Font.system(size: 16, weight: .semibold)
+    
+    static let subheadingRegular = Font.system(size: 15, weight: .regular)
+    static let subheadingMedium = Font.system(size: 15, weight: .medium)
+    
+    static let footnoteRegular = Font.system(size: 13, weight: .regular)
+    static let footnoteSemibold = Font.system(size: 13, weight: .semibold)
+    
+    static let caption1Light = Font.system(size: 12, weight: .light)
+    static let caption1Medium = Font.system(size: 12, weight: .medium)
+    
+    static let caption2Light = Font.system(size: 11, weight: .light)
+    static let caption2Semibold = Font.system(size: 11, weight: .light)
+}
