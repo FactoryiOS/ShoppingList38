51fbdd43192034a9a9e169f7383197b099616b88
elmkh1IL <el.mkh@inbox.ru>
2026-09-23 20:20:45 +0300

    [Fix] Fix layout spacing and add colors

--- ShoppingList38/Screens/WelcomeView.swift
+++ ShoppingList38/Screens/WelcomeView.swift
@@ -11,43 +11,51 @@ struct WelcomeView: View {
     
     let onStart: () -> Void
     
-    var  body: some View {
-        VStack {
+    var body: some View {
+        VStack(spacing: 0) {
             Text("Добро пожаловать!")
                 .font(AppTypography.largeTitleRegular)
-                .padding(.top, 32)
+                .foregroundStyle(.primaryText)
+                .frame(maxWidth: .infinity)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 16)
             
             Image(.welcome)
                 .resizable()
                 .scaledToFit()
-                .frame(maxWidth: 330)
-                .padding(.top, 52)
-            
-            Spacer()
+                .padding(.horizontal, 49)
+                .padding(.top, 48)
+                .padding(.bottom, 48)
             
             VStack(spacing: 16) {
-                Text("Никогда не забывайте, \nчто нужно купить")
+                Text("Никогда не забывайте,\nчто нужно купить")
                     .font(AppTypography.title2semibold)
+                    .foregroundStyle(.primaryText)
                     .multilineTextAlignment(.center)
                 
-                Text("Создавайте списки \nи не переживайте о покупках")
+                Text("Создавайте списки\nи не переживайте о покупках")
                     .font(AppTypography.body)
+                    .foregroundStyle(.primaryText)
                     .multilineTextAlignment(.center)
             }
+            
+            .padding(.horizontal, 16)
             
             Spacer()
             
             AppButton(title: "Начать", isActive: true) {
                 onStart()
             }
-            .padding(.horizontal, 24)
-            .padding(.bottom, 16)
+            
+            .padding(.horizontal, 16)
+            .padding(.bottom, 20)
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.appBackground.ignoresSafeArea())
     }
 }
 
-#Preview {
-    WelcomeView {
-        print("Начать")
-    }
+#Preview("Light") {
+    WelcomeView { }
+        .preferredColorScheme(.light)
 }
